a332f15c08e3cebfcb2b56825b4675c46543cf62
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2021-03-12 07:01:50 +0530

    AnimatedBuilder added

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -126,22 +126,24 @@ class _AuthCardState extends State<AuthCard>
       duration: Duration(milliseconds: 300),
     );
 
-    _heightAnimation = Tween<Size>(begin: Size(double.infinity, 300),end: Size(double.infinity, 350)).animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
+    _heightAnimation = Tween<Size>(
+            begin: Size(double.infinity, 300), end: Size(double.infinity, 350))
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
 
-    _heightAnimation.addListener(() {
-      setState(() {
-              
-            });
-    });
+    // _heightAnimation.addListener(() {
+    //   setState(() {
+
+    //         });
+    // });
   }
 
   @override
-    void dispose() {
-      // TODO: implement dispose
+  void dispose() {
+    // TODO: implement dispose
 
-      super.dispose();
-      _controller.dispose();
-    }
+    super.dispose();
+    // _controller.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -239,14 +241,17 @@ class _AuthCardState extends State<AuthCard>
         borderRadius: BorderRadius.circular(10.0),
       ),
       elevation: 8.0,
-      child: Container(
-        // height: _authMode == AuthMode.Signup ? 350 : 300,
-        height: _heightAnimation.value.height,
-        constraints:
-            BoxConstraints(minHeight: _heightAnimation.value.height),
+      child: AnimatedBuilder(
+        animation: _heightAnimation,
+        builder: (ctx, ch) => Container(
+            // height: _authMode == AuthMode.Signup ? 350 : 300,
+            height: _heightAnimation.value.height,
+            constraints:
+                BoxConstraints(minHeight: _heightAnimation.value.height),
             //  _authMode == AuthMode.Signup ? 350 : 300
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
+            width: deviceSize.width * 0.75,
+            padding: EdgeInsets.all(16.0),
+            child: ch),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
