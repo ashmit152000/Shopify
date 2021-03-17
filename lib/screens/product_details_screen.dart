a4f0f2956b265d78f31b9495a74e75fb88bd57f2
Ashmit Raj <ashmitraj@Ashmits-MacBook-Air.local>
2021-03-17 19:59:59 +0530

    added slivers in the app

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -11,64 +11,72 @@ class ProductDetailsScreen extends StatelessWidget {
     final productData = Provider.of<ProductProviders>(context);
     final product = productData.getProduct(id);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(product.title),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              width: double.infinity,
-              child: Expanded(
-                  child: Hero(
-                    tag: product.id,
-                    child: Image.network(
-                product.imageUrl,
-                fit: BoxFit.cover,
-              ),
-                  )),
-              height: 350,
-              alignment: Alignment.center,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              
-              padding: EdgeInsets.all(10),
-              width: double.infinity,
-              alignment: Alignment.center,
-              child: Text(product.description, style: TextStyle(fontSize: 15),softWrap: true,),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Card(
-              child: Padding(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  children: [
-                    Text(
-                      'Amount: ',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    Card(
-                      child: Padding(
-                        padding: EdgeInsets.all(5),
-                        child: Text(
-                          '\$${product.price}',
-                          style: TextStyle(color: Colors.white, fontSize: 15),
-                        ),
-                      ),
-                      color: Theme.of(context).primaryColor,
-                    )
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      // appBar: AppBar(
+      //   title: Text(product.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            expandedHeight: 350,
+            pinned: true,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(product.title),
+              background: Hero(
+                tag: product.id,
+                child: Image.network(
+                  product.imageUrl,
+                  fit: BoxFit.cover,
                 ),
               ),
-            )
-          ],
-        ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                padding: EdgeInsets.all(10),
+                width: double.infinity,
+                alignment: Alignment.center,
+                child: Text(
+                  product.description,
+                  style: TextStyle(fontSize: 15),
+                  softWrap: true,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Card(
+                child: Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Amount: ',
+                        style: TextStyle(fontSize: 15),
+                      ),
+                      Card(
+                        child: Padding(
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            '\$${product.price}',
+                            style: TextStyle(color: Colors.white, fontSize: 15),
+                          ),
+                        ),
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      
+                    ],
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  ),
+                ),
+              ),
+              SizedBox(height: 800,),
+            ]),
+          ),
+        ],
       ),
     );
   }
